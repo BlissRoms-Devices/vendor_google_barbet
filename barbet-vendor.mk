a704a3b54268381af3951ad2d56949f2fd5f1dae
Jackeagle <jackeagle102@gmail.com>
2023-01-16 13:14:18 -0500

    barbet: Kill DeviceIntelligenceNetworkPrebuilt & HbmSVManager
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- barbet-vendor.mk
+++ barbet-vendor.mk
@@ -453,7 +453,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel2020 \
     DiagMon \
     EuiccGoogle \
@@ -475,7 +474,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     PixelNfc \
     RilConfigService \
